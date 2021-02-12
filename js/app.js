46393a35454f72f892b1591dd093e9c2b09a55b5
bhakti0699 <poojalalcheta@gmail.com>
2021-02-12 12:59:42 +0530

    normal chnages

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ function showTask(taskVal)
     let addedTaskList = document.getElementById("addedtasklist");
     taskVal.forEach((item, index) => {
         html += `<tr>
-                    <th scope="row">${index+1}</th>
                     ${item.status=="true" ? `<td class="completed">${item.task}</td>`:`<td>${item.task}</td>`}
                     <td><button type="button" class="edit text-primary" id=${item.id}><i class="fa fa-edit"></i>Edit</button></td>
                     <td><button type="button" class="check text-success" id=${item.id}><i class="fa fa-check-square-o"></i>Complete</button></td>
