9720149e04b10573453eccf6c224308e7ffe8e50
bhakti0699 <poojalalcheta@gmail.com>
2021-02-19 11:04:54 +0530

    normal

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function showTask(taskVal)
     let html = ``;
     let addedTaskList = document.getElementById("addedtasklist");
     taskVal.forEach((item) => {
-        html += `<tr>
+        html += `<tr class="ui-sortable-handle">
                     ${item.status=="true" ? `<td><input type="checkbox" id="${item.id}" class="checked" checked/></td><td class="completed">${item.task}</td>`:`<td><input type="checkbox" id="${item.id}" class="checked"/></td><td>${item.task}</td>`}
                     <td><button type="button" class="edit text-primary" id=${item.id}><i class="fa fa-edit"></i>Edit</button></td>
                     <td><button type="button" class="trash text-danger trash" id=${item.id}><i class="fa fa-trash"></i>Delete</button></td>
